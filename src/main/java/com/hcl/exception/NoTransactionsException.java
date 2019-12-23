c621ee484e7e5af7f2416efec8e5c30f9f7b8e9a
vinod-nagaraj <57495482+vinod-nagaraj@users.noreply.github.com>
2019-12-23 13:37:13 +0530

    Create NoTransactionsException.java

---
+++ src/main/java/com/hcl/exception/NoTransactionsException.java
@@ -0,0 +1,12 @@
+package com.hcl.exception;
+
+public class NoTransactionsException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+
+	public NoTransactionsException(String message)
+	{
+		super(message);  
+	}
+
+}
